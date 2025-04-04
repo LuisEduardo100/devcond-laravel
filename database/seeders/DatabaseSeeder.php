23ef0d7fc79aa1d2ff2b2648072d82d1bb7affcf
Luis Eduardo <luisedu.lepa@gmail.com>
2025-04-04 20:01:42 -0300

    chore: criar seeders para popular o banco de dados com unidades, quadro de avisos e areas (units, walls, areas)

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +14,55 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        DB::table('units')->insert([
+            'name' => 'AP 100',
+            'id_owner' => '1'
+        ]);
+        DB::table('units')->insert([
+            'name' => 'AP 200',
+            'id_owner' => '1'
+        ]);
+        DB::table('units')->insert([
+            'name' => 'AP 300',
+            'id_owner' => ''
+        ]);
+        DB::table('units')->insert([
+            'name' => 'AP 400',
+            'id_owner' => ''
+        ]);
+        DB::table('areas')->insert([
+            'allowed' => 'AP 400',
+            'title' => 'Academia',
+            'cover' => 'gym.jpg',
+            'days' => '1,2,4,5',
+            'start_time' => '08:00:00',
+            'end_time' => '18:00:00'
+        ]);
+        DB::table('areas')->insert([
+            'allowed' => 'AP 400',
+            'title' => 'Piscina',
+            'cover' => 'pool.jpg',
+            'days' => '1,2,3,4,5',
+            'start_time' => '08:00:00',
+            'end_time' => '18:00:00'
+        ]);
+        DB::table('areas')->insert([
+            'allowed' => 'AP 400',
+            'title' => 'Área de lazer',
+            'cover' => 'playground.jpg',
+            'days' => '1,2,3,4,5',
+            'start_time' => '08:00:00',
+            'end_time' => '18:00:00',
+        ]);
+        DB::table('walls')->insert([
+            'title' => 'Título de aviso de teste',
+            'body' => 'Lorem Ipsum',
+            'date_created' => '2020-12-20 15:00:00'
+        ]);
+        DB::table('walls')->insert([
+            'title' => 'Alerta geral para todos',
+            'body' => 'Lorem Ipsum',
+            'date_created' => '2020-12-20 15:00:00'
         ]);
     }
 }
